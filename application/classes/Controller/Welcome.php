e519c802c71568125e92d16cacfb885f94236ff0
sydorenkovd <sydorenkovd@gmail.com>
2016-06-20 09:09:20 +0300

    init

---
+++ application/classes/Controller/Welcome.php
@@ -0,0 +1,15 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Controller_Welcome extends Controller {
+
+	public function action_index()
+	{
+		$this->response->body('hello, world!');
+	}
+
+    public function action_my(){
+        $description_main = Kohana::$config->load('mysite.description.main');
+        $description_alt = Kohana::$config->load('mysite.description.alt');
+        echo $description_main . '<br>' . $description_alt;
+    }
+}
---
+++ application/config/mysite.php
@@ -0,0 +1,8 @@
+<?php defined('SYSPATH') or die('No direct script access');
+return array(
+    'title'       => 'Мой первый сайт на Кохане',
+    'description' => array(
+        'main' => 'Самый лучший сайт на свете',
+        'alt'  => 'Пока не будет создан второй сайт'
+    )
+);
