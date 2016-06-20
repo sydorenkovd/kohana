782410519c96bfeffbdfaa81c4841fbc78c7563c
sydorenkovd <sydorenkovd@gmail.com>
2016-06-20 21:12:23 +0300

    database work

--- application/classes/Controller/Comments.php
+++ application/classes/Controller/Comments.php
@@ -34,5 +34,30 @@ class Controller_Comments extends Controller {
 
         $this->response->body($content);
     }
+public function action_create(){
+    if($_POST)
+    {
+        $_POST = Arr::map('trim', $_POST);
 
+        $post = Validation::factory($_POST);
+        $post -> rule('user', 'not_empty')
+            -> rule('user', 'min_length', array(':value', 2))
+            -> rule('user', 'max_length', array(':value', 20))
+            -> rule('email', 'email')
+            -> rule('message', 'not_empty')
+            -> rule('message', 'max_length', array(':value', 100));
+
+        if($post -> check())
+        {
+            Model::factory('Comment')
+                ->create_comment($article_id, $_POST['user'], $_POST['message']);
+
+            $uri = Request::detect_uri();
+            Request::initial()->redirect($uri);
+
+        }
+        else
+            $errors = $post -> errors('validation');
+    }
+}
 }
--- application/views/comments/show.php
+++ application/views/comments/show.php
@@ -1,9 +1,33 @@
-<?php foreach($comments as $comment): ?>
-
-    <strong>Имя пользователя:</strong><br />
-    <?php echo $comment['name']; ?><br />
-    <strong>Комментарий пользователя:</strong><br />
-    <?php echo $comment['comment']; ?>
-    <br /><hr /><br />
-
-<?php endforeach; ?>
+<form action="" method="post">
+    <table width="500" border="0" cellspacing="5" cellpadding="5">
+        <tr>
+            <td>
+                Ваше имя: <br />
+                <input name="user" type="text" value="<?php echo HTML::chars(Arr::get($_POST, 'user')); ?>" />
+            </td>
+            <td>
+                <strong style="color:#f00;"><?php if(isset($errors['user'])) echo $errors['user']; ?></strong>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                Ваш e-mail: <br />
+                <input name="email" type="text" value="<?php echo HTML::chars(Arr::get($_POST, 'email')); ?>" />
+            </td>
+            <td>
+                <strong style="color:#f00;"><?php if(isset($errors['email'])) echo $errors['email']; ?></strong>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                Сообщение: <br />
+     <textarea name="message" cols="25" rows="5"><?php echo HTML::chars(Arr::get($_POST, 'message')); ?>
+</textarea>
+            </td>
+            <td valign="middle">
+                <strong style="color:#f00;"><?php if(isset($errors['message'])) echo $errors['message']; ?></strong>
+            </td>
+        </tr>
+    </table>
+    <input name="send" type="submit" value="Отправить" />
+</form>
